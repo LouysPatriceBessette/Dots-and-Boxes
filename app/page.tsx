94a39488c97b965d3ad9213e031b843f112e05ef
Louys Patrice Bessette <louys.patrice.bessette@gmail.com>
2026-01-13 16:53:10 -0500

    FOUC handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from "react";
 import { StrictMode } from "react";
 import { Provider } from 'react-redux';
 import { store } from "./store/store";
@@ -8,6 +9,16 @@ import { SocketListen } from "./socket";
 import { ChakraProvider } from "./components/Chakra/ChakraProvider/ChakraProvider";
 
 export default function Home() {
+
+  // FOUC handling
+  useEffect(() => {
+    setTimeout(() => {
+      const body = document.querySelector('body')
+      if(body){
+        body.classList.add('loaded')
+      }
+    }, 600)
+  }, [])
   return (
     <StrictMode>
       <Provider store={store}>
